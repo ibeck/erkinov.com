72dea2bd8313cfba215f118b6a2fd9c34ddd22e5
Ibeck <ibeck@AIBEKs-iMac.local>
2019-05-29 09:42:56 +0300

    deletestuff

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,8 +1,6 @@
 import React from "react"
 import { Link } from "gatsby"
-
 import Layout from "../components/layout"
-import Image from "../components/image"
 import SEO from "../components/seo"
 
 const IndexPage = () => (
@@ -10,9 +8,7 @@ const IndexPage = () => (
     <SEO title="Home" />
     <h1>Hi people</h1>
     <p>This is my personnal website</p>
-    <div style={{ maxWidth: `300px`, marginBottom: `1.45rem` }}>
-      <Image />
-    </div>
+
     <Link to="/page-2/">Watch Video on  page 2</Link>
   </Layout>
 )
